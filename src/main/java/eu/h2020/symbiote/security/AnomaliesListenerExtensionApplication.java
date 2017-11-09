1c9ba99e7cec3d870180573f8db2114da2839caf
Piotr Jakubowski <pjakub@man.poznan.pl>
2017-11-09 11:33:51 +0100

    Added anomaly handling functionality

---
+++ src/main/java/eu/h2020/symbiote/security/AnomaliesListenerExtensionApplication.java
@@ -0,0 +1,12 @@
+package eu.h2020.symbiote.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomaliesListenerExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnomaliesListenerExtensionApplication.class, args);
+	}
+}
